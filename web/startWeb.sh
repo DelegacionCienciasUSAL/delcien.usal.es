b4a3cc152a578e03a75b26d1a57150095e28b0aa
Janchorizo <id00681749@usal.es>
2018-12-06 20:17:34 +0100

    Fixed initial table creation for the database

--- web/startWeb.sh
+++ web/startWeb.sh
@@ -4,8 +4,9 @@
 echo "Starting Gunicorn."
 cd /src/
 python3 manage.py makemigrations
-python3 manage.py migrate
+python3 manage.py migrate --run-syncdb
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python3 manage.py shell
 python3 manage.py collectstatic --noinput
 exec gunicorn delcien.wsgi:application \
     --bind 0.0.0.0:8000 \
-    --workers 3
+    --workers 3 
