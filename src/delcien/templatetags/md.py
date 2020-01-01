cbd58c2afa353f49b78b7c8329c1a23a860f52d3
Janchorizo <jancho@usal.es>
2020-01-01 21:59:53 +0100

    Add markdown rendering support.

---
+++ src/delcien/templatetags/md.py
@@ -0,0 +1,12 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+import markdown as md
+
+register = template.Library()
+
+@register.filter(name="markdown")
+@stringfilter
+def markdown(value):
+    return md.markdown(value, extensions=['markdown.extensions.fenced_code'])
+
