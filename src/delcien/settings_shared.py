ddffc8a402fcd45ec67f992160cc4d3391876bd8
Janchorizo <jancho@usal.es>
2020-01-03 13:19:10 +0100

    Remove secret key token.

--- src/delcien/settings_shared.py
+++ src/delcien/settings_shared.py
@@ -100,5 +100,4 @@ MEDIA_ROOT = '/var/www/delcien/uploads/'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-SECRET_KEY = ''
+EMAIL_HOST_PASSWORD = '
