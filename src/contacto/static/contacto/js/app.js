57a1584119c7a502e3f688bb361e666f97c94d61
Janchorizo <jancho@usal.es>
2020-01-03 10:19:32 +0100

    Style contact page.

---
+++ src/contacto/static/contacto/js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+  [].forEach.call(document.querySelectorAll('.dropimage'), function(img){
+    img.onchange = function(e){
+      var inputfile = this, reader = new FileReader();
+      reader.onloadend = function(){
+        inputfile.style['background-image'] = 'url('+reader.result+')';
+      }
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  });
+});
