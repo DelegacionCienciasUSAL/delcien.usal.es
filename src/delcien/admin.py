13cc569d1a1ae24b790e8cc7060518d82676e1b7
Janchorizo <jancho@usal.es>
2020-01-03 11:09:38 +0100

    Change delcien models to handle varying social media.

--- src/delcien/admin.py
+++ src/delcien/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Delegacion
+from .models import Delegacion, RedSocial
 
 class DelegacionAdmin(admin.ModelAdmin):
     def has_add_permission(self, *args, **kwargs):
         return not Delegacion.objects.exists()
         
 admin.site.register(Delegacion, DelegacionAdmin)
+admin.site.register(RedSocial)
--- src/delcien/models.py
+++ src/delcien/models.py
@@ -1,16 +1,41 @@
 from django.db import models
 
+
+
+
+
+
+class RedSocial(models.Model):
+    SOCIAL_ICON_CHOICES = (
+        ('fi-xnsuxl-twitter', 'Twitter'),
+        ('fi-xnsuxl-instagram', 'Instagram'),
+        ('fi-xnsuxl-facebook-circle', 'Facebook'),
+        ('fi-xnsuxl-github', 'Github'),
+        ('fi-xnsuxl-youtube', 'Youtube'),
+        ('fi-xnsuxl-twitch-glitch', 'Twitch'),
+    )
+    social_max_length = max(map(lambda x: len(x[0]), SOCIAL_ICON_CHOICES))
+
+    red = models.CharField( max_length=150)
+    perfil = models.CharField( max_length=150)
+    icono = models.CharField( max_length=social_max_length, choices=SOCIAL_ICON_CHOICES, default='fi-xnsuxl-twitter')
+
+    class Meta:
+        verbose_name = 'Redes Sociales'
+        verbose_name_plural = 'Redes Sociales'
+
+    def __str__(self):
+        return(self.red)
+
 class Delegacion(models.Model):
     facultad = models.CharField( max_length=150)
-    twitter = models.CharField( max_length=150)
-    github = models.CharField( max_length=150)
-    instagram = models.CharField( max_length=150)
-    youtube = models.CharField( max_length=150)
-    twitch = models.CharField( max_length=150)
-    facebook = models.CharField( max_length=150)
     direccion = models.CharField( max_length=150)
     telefono = models.CharField( max_length=150)
     email = models.CharField( max_length=150)
 
-    def __str_( self):
+    class Meta:
+        verbose_name = 'Detalles de la Delegacion'
+        verbose_name_plural = 'Detalles de la Delegacion'
+
+    def __str__( self):
         return(f"Delegacion de estudiantes ({self.facultad})")
