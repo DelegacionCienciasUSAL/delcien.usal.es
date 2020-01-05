56031ae41896554d911f4bb88d15e67f189171c5
Janchorizo <jancho@usal.es>
2020-01-05 11:34:06 +0100

    Change model for the news app to allow article editing.

--- src/actualidad/models.py
+++ src/actualidad/models.py
@@ -1,23 +1,7 @@
 from django.db import models
+import markdownx
 
 class Noticia( models.Model):
-	FACULTAD = 'FAC'
-	INGINFORMATICA = 'INF'
-	INGQUIMICA = 'IQI'
-	FISICA = 'FIS'
-	ESTADISTICA = 'EST'
-	MATEMATICAS = 'MAT'
-	GEOLOGIA = 'GEO'
-	OPCIONES_AMBITO = (
-		( FACULTAD, 'Facultad'),
-		( INGINFORMATICA, 'Ingeniería Informática'), 
-		( INGQUIMICA, 'Ingeniería Química'), 
-		( FISICA, 'Física'), 
-		( ESTADISTICA, 'Estadística'), 
-		( MATEMATICAS, 'Matemáticas'), 
-		( GEOLOGIA, 'Geología'),	 
-	)
-
 	EVENTOS = 'EVE'
 	FORMACION = 'FOR'
 	ACADEMICA = 'ACA'
@@ -33,11 +17,8 @@ class Noticia( models.Model):
 
 	titulo = models.CharField( max_length=30) 
 	categoria = models.CharField( max_length=3, choices=OPCIONES_CATEGORIA)
-	ambito = models.CharField( max_length=3, choices=OPCIONES_AMBITO)
-	fecha = models.DateField( blank=True, null=True, verbose_name='Fecha')
-	lugar = models.CharField( max_length=40, blank=True)
-	referencia = models.URLField( blank=True)
-	noticia = models.CharField( max_length=150)
+	fecha = models.DateField( auto_now_add=True, verbose_name='Fecha')
+	noticia = markdownx.models.MarkdownxField()
 
 	def __str_( self):
-		return( "%s : %s %s : %s"%( self.titulo, self.ambito, self.categoria, self.fecha))
+		return( "%s : %s : %s"%( self.titulo, self.categoria, self.fecha))
