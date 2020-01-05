68448df0bb8f6d87bcd7f8b7a312d57a998cd726
Janchorizo <jancho@usal.es>
2020-01-05 11:35:48 +0100

    Finish global navigation.

--- src/delcien/context_preprocessors.py
+++ src/delcien/context_preprocessors.py
@@ -1,7 +1,13 @@
 from delcien.models import Delegacion, RedSocial
+from delcien.urls import URL_TO_NAME
 
 def delegation_and_social_accounts(request):
+    base_path = 'inicio' if request.path == '/' else request.path.split('/')[1]
+    app_name = URL_TO_NAME[base_path]
+
     return {
         'delegacion': Delegacion.objects.first(),
-        'social': RedSocial.objects.all()
+        'social': RedSocial.objects.all(),
+        'base_path': base_path,
+        'app_name': app_name,
     }
