b2ee13cc85aec2993fa652b2c38a06d48b2eeaf6
Janchorizo <jancho@usal.es>
2020-01-07 22:05:36 +0100

    Update home page to show latest news and next events.

--- src/inicio/views.py
+++ src/inicio/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import Http404
 from inicio.models import Alerta, Seccion, PaginaInicio
 from delcien.models import Delegacion
+from actualidad.models import Articulo, Evento
 
 # Create your views here.
 def main( request):
@@ -10,11 +11,16 @@ def main( request):
     contenido = PaginaInicio.objects.first()
     secciones = Seccion.objects.all()
 
+    articulos = Articulo.objects.order_by('-id')[:3]
+    eventos = Evento.objects.order_by('fecha')[:3]
+
     context = {
         'titulo' : 'Inicio', 
         'alertas': alertas, 
         'delegacion': datos_delegacion, 
         'contenido': contenido,
-        'sections': secciones
-        }
+        'sections': secciones,
+        'articulos': articulos,
+        'eventos': eventos,
+    }
     return( render( request, 'inicio/main.html', context))
